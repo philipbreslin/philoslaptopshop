4c8dc704935a142dd77243b25d3b57135168ca96
Philip Breslin <philipbreslin@hotmail.com>
2013-10-02 18:11:18 +0100

    first shop commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  before_filter :is_admin?
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to products_path, :notice => 'User creation successful!'
+    else
+      render :action => 'new'
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Philoslaptopshop::Application.config.secret_token = '2dbf2ab742a217223318526c0e724ebec11b4d6fb98e4960b441dff2a22ee6605b7d814c74594d56ba9ad3febd9f19e21c8446219db2ea5fc5de1dcbafed4baf'
